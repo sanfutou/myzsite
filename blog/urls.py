a5bb59e2e792ab41392557b3a0f9cf6f9df33e39
sanfutou <723478279@qq.com>
2018-02-26 22:21:05 +0800

    学习到34页，但是博客文章还看不见。

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r"^$",views.blog_title, name="blog_title"),
+#    url(r"^$",views.blog_title, name="blog_title"),
+    url(r'^$',views.blog_title,name="blog_title"),
+    url(r'(?P<article_id>\d)/$',views.blog_article,name="blog_detail"),
 ]
 app_name='blog'
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 # Create your views here.
 from .models import BlogArticles
@@ -6,3 +6,12 @@ from .models import BlogArticles
 def blog_title(request):
     blogs=BlogArticles.objects.all()
     return render(request,"blog/titles.html",{"blogs":blogs})
+
+def blog_article(request,article_id):
+    article=BlogArticles.objects.get(id=article_id)
+    pub=article.puplish
+    return render(request,"blog/content.html",{"article":article,"publish":pub})
+def blog_article(request,article_id):
+    article=get_object_or_404(BlogArticles,id=article_id)
+    pub=article.puplish
+    return render(request,"blog/content.html",{"article":article,"publish":pub})
