2d3e2abd755be3ff7a0ac73090e8550a062bce81
sanfutou <723478279@qq.com>
2018-03-01 23:08:57 +0800

    学到57页，问题版

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import  url
 from . import views
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 urlpatterns =[
-    url(r'^login/$',views.user_login, name="user_login"),
+#    url(r'^login/$',views.user_login, name="user_login"),
+    url(r'^login/$', auth_views.login, name="user_login"),
 ]
 app_name='account'
